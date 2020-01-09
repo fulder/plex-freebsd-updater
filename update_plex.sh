7c784c1c20de8a84265a4d653171e32b3ca83c82
fulder <misad90@gmail.com>
2020-01-09 22:57:34 +0100

    First version of script

---
+++ update_plex.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+version=1.18.4.2171-ac2afe5f8
+
+echo "Downloading plex server with version: ${version}"
+#wget "https://downloads.plex.tv/plex-media-server-new/${version}/freebsd/PlexMediaServer-${version>
+
+echo "Unpacking zip"
+tar xzf "PlexMediaServer-${version}-FreeBSD-amd64.tar.bz2"
+
+echo "Updating current plex"
+rm -rf PlexMediaServer
+mv "./PlexMediaServer-${version}" PlexMediaServer
+
+#Cleanup
+rm -r "PlexMediaServer-${version}"
+rm "PlexMediaServer-${version}-FreeBSD-amd64.tar.bz2"
